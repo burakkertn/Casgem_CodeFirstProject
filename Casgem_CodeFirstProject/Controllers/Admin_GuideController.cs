d476c1947461781194d230b046b893ed0eb12e77
Burak <99614754+burakkertn@users.noreply.github.com>
2023-07-13 11:06:03 +0300

    Add project files.

---
+++ Casgem_CodeFirstProject/Controllers/Admin_GuideController.cs
@@ -0,0 +1,64 @@
+﻿using Casgem_CodeFirstProject.DAL.Context;
+using Casgem_CodeFirstProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace Casgem_CodeFirstProject.Controllers
+{[Authorize]
+    public class Admin_GuideController : Controller
+    {
+        
+        TravelContext travelContext = new TravelContext();
+        // GET: Admin_Guide
+        public ActionResult Index()
+        {
+            var values = travelContext.Guides.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddGuide()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddGuide(Guide p)
+        {
+            travelContext.Guides.Add(p);
+            travelContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteGuide(int id)
+        {
+            var values =travelContext.Guides.Find(id);
+            travelContext.Guides.Remove(values);
+            travelContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult GuideUpdate(int id)
+        {
+            var values = travelContext.Guides.Find(id);
+
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult GuideUpdate(Guide p)
+        {
+            var values = travelContext.Guides.Find(p.GuideID);
+            values.GuideName = p.GuideName;
+            values.GuideTitle = p.GuideTitle;
+            values.GuideImageUrl = p.GuideImageUrl;
+           
+            travelContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Casgem_CodeFirstProject/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Casgem_CodeFirstProject.DAL.Context;
+using Casgem_CodeFirstProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+
+namespace Casgem_CodeFirstProject.Controllers
+{
+
+    public class LoginController : Controller
+    {
+        TravelContext travelContext = new TravelContext();
+        [HttpGet]
+     
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+          
+            var user = travelContext.Admins.FirstOrDefault(x => x.Username == admin.Username && x.Password == admin.Password);
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(user.Username, false);
+                Session["usertravel"] = user.Username.ToString();
+                return RedirectToAction("Index", "AdminGuide");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+    }
+}
